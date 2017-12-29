2c20f80c73d85ca3013f8c44a2c1d447e9357564
amandre <andrzejamghar@gmail.com>
2017-12-30 00:10:00 +0100

    Calculate potential path, shortest path, sorting algorithm and small fixes

--- optimization.py
+++ optimization.py
@@ -7,15 +7,16 @@ import numpy
 '''
 assumptions:
  - setupCost = 0 (for existing edge) or setupCost = calculate_cost(city1,city2) (for new edge)
- - capacity = 1 (by default each path uses only one edge)
- #EDIT - cap = 10 (def cap=1)
+ - capacity = 1 (by default each path could use many edges and may consist of many vertices)
+ // edit cap=10 (def cap=1)
 '''
 
-cap = 10 # link (module) capacity [Gbps]
-#global budget
+cap = 1 # module capacity [Gbps]
+u_max = 10 #max number of modules per link are allowed
 budget = 100000
 
-def calculate_cost (graph, city1,city2):
+
+def calculate_cost (graph, city1, city2):
     # calculates cost of a link added between 2 locations
     # calculates distance in kilometers based on Cartesian coordinates
     # x describes East or West, y describes North or South
@@ -34,6 +35,7 @@ def calculate_cost (graph, city1,city2):
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
     distance = int(R * c/1000) # distance in kilometers
     return distance^2 # cost of a newly installed link between 2 locations
+
 
 def draw_graph(xml_file, cap):
     # builds a graph with existing vertices and edges
@@ -62,36 +64,60 @@ def draw_graph(xml_file, cap):
         num1 = graph.node[e[1].text]['no']-1
         graph.add_edge(e[0].text,e[1].text,id=e.attrib['id'],d=demand[num0][num1],capacity=cap,setupCost=0)
         graph.add_edge(e[1].text,e[0].text,id=e.attrib['id'],d=demand[num1][num0],capacity=cap,setupCost=0)
-   # calculate_potential_path(graph.edges)
+    #calculate_potential_path(graph.edges)
     nx.draw(graph)
     return graph
 
 
-def calculate_potential_path(graph, edges,budget):
-    for city1, city2, attr in edges:
-        if attr['d']>attr['capacity']:
-            lack_of_cap=attr['d']-attr['capacity']
-            print lack_of_cap
-            pot_best_path=graph.add_edge(city1,city2,capacity=cap+lack_of_cap)
-            budget -= calculate_cost(graph,city1,city2)
-            print budget
-        else:
-            print "Link is able to handle the demand"
-#            pot_best_path=...
-#            print "Problem unsolvable"
-#        return e.cost
+def calculate_potential_path(graph, budget, demands):
+    pot_budget = budget
+    for city1, city2, attr in demands:
 
-# def set_limitations():
-#     # adds limitations to the model, ensures that the requirements for the system are fulfilled
-#     u_max = 9 #max 9 modules per link are allowed
-#     for v in graph.nodes:
-#         print graph.edges.
-#         sum(graph.edges)
+        modules_no = 1
+        if attr['d'] > attr['capacity']:
+            total_edge_cap = attr['capacity']
+            lack_of_cap = attr['d'] - attr['capacity']
+#            pot_best_path=graph.add_edge(city1,city2,capacity=cap+lack_of_cap)
+            while lack_of_cap>0:
+                if modules_no <= u_max:
+                    print "lackOfCap=", lack_of_cap
+                    graph.add_edge(city1, city2, capacity=cap)
+                    lack_of_cap -= cap
+                    pot_budget -= calculate_cost(graph, city1, city2) # calculated amount of budget left when the direct link between two cities is needed
+                    total_edge_cap += cap
+                    print "totalEdgeCap=", total_edge_cap
+                    modules_no += 1
+                else:
+                    print "Problem unsolvable, all ", u_max ," modules are already used"
+                    break
+            print pot_budget
+        else:
+            print "Link is able to handle the demand.\nCreating a direct link for (", city1, ",", city2, ")..."
+#           pot_best_path=...
+            graph.add_path([city1, city2])
+
+
+def calculate_shortest_path(graph, src, dst):
+    try:
+        dp = nx.dijkstra_path(graph, src, dst)
+    except nx.exception.NetworkXNoPath:
+        raise Exception('No path exists between source and target')
+    return dp
+
+
+def sort_demands_desc(graph):
+    # sorts the list of edges based on the demand value in descending order
+    return sorted(graph.edges.data(), key=lambda x: x[-1], reverse=True)
 
 if __name__ == "__main__":
     graph = draw_graph('polska.xml', cap)
-    print graph.edges(data=True)
-    calculate_potential_path(graph,graph.edges(data=True),budget)
-#    calculate_cost(graph,"Krakow","Bialystok")
-#    plt.show()
-    # print graph.edges('Bydgoszcz', 'Kolobrzeg')
+    sorted_demands = sort_demands_desc(graph)
+   # calculate_potential_path(graph, budget, sorted_demands)
+    paths = []
+    for src in graph.nodes:
+        for dst in graph.nodes:
+            path = calculate_shortest_path(graph, src, dst)
+            if len(path) > 1:
+                paths.append(path)
+#    print paths
+    plt.show()
